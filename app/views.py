e8a1b04cecebae1caa3b84c693f36db79839b3d4
Jonathan Alderson <sc17j3a@leeeds.ac.uk>
2019-02-08 12:55:35 +0000

    Inital Browse Page

--- app/views.py
+++ app/views.py
@@ -4,3 +4,8 @@ from app import app
 @app.route('/')
 def index():
     return render_template("home.html")
+
+
+@app.route('/browse')
+def browse():
+    return render_template("browse.html") # this is for the shop page
