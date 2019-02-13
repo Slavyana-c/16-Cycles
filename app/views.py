2ad64e6df567ffc3b0d37e4345c9cd59a6e99055
Jonathan Alderson <sc17j3a@leeeds.ac.uk>
2019-02-13 14:09:13 +0000

    QR Code now works with any link, and will be attached to the sendt, formatted email

--- app/views.py
+++ app/views.py
@@ -7,6 +7,7 @@ import smtplib
 from email.MIMEMultipart import MIMEMultipart
 from email.MIMEText import MIMEText
 from email.MIMEBase import MIMEBase
+from email.MIMEImage import MIMEImage
 from email import encoders
 
 # all imports for QR Code Generation
@@ -39,14 +40,9 @@ def qr():
 
     # generating the QR code
 
-    url = pyqrcode.create('http://uca.edu')
-    print(url.text())
+    url = pyqrcode.create('https://ksassets.timeincuk.net/wp/uploads/sites/55/2016/07/2015_PeepShow_Mark2_Press_111115-920x610.jpg')
+    url.png('app/qrCode.png', scale=6, module_color=[0, 0, 0, 128], background=[0xff, 0xff, 0xcc])
 
-    qrCode = url.text()
-    qrCode = qrCode.replace("1","#")
-    qrCode = qrCode.replace("0"," ")
-
-    print(qrCode)
 
 
 
@@ -63,65 +59,75 @@ def qr():
 
 
     html = """\
-<html>
-<head>
-	<title></title>
-	<link href="https://svc.webspellchecker.net/spellcheck31/lf/scayt3/ckscayt/css/wsc.css" rel="stylesheet" type="text/css" />
-	<link href="https://svc.webspellchecker.net/spellcheck31/lf/scayt3/ckscayt/css/wsc.css" rel="stylesheet" type="text/css" />
-	<link href="https://svc.webspellchecker.net/spellcheck31/lf/scayt3/ckscayt/css/wsc.css" rel="stylesheet" type="text/css" />
-</head>
-<body aria-readonly="false" style="cursor: auto;">
-<h3><span style="font-family:arial,helvetica,sans-serif">Thank you for renting with us today</span></h3>
-<span style="font-family:arial,helvetica,sans-serif">Please find attached below your recipt<br />
-<br />
-Username : jonathanAlderson<br />
-Checkout&nbsp; : 15/02/19<br />
-More detail: loren&nbsp;ipsum<br />
-dolor eptium: pera equatus<br />
-pectar melao: perus deulium</span>
+        <html>
+        <head>
+        	<title></title>
+        	<link href="https://svc.webspellchecker.net/spellcheck31/lf/scayt3/ckscayt/css/wsc.css" rel="stylesheet" type="text/css" />
+        	<link href="https://svc.webspellchecker.net/spellcheck31/lf/scayt3/ckscayt/css/wsc.css" rel="stylesheet" type="text/css" />
+        	<link href="https://svc.webspellchecker.net/spellcheck31/lf/scayt3/ckscayt/css/wsc.css" rel="stylesheet" type="text/css" />
+        </head>
+        <body aria-readonly="false" style="cursor: auto;">
+        <h3><span style="font-family:arial,helvetica,sans-serif">Thank you for renting with us today</span></h3>
+        <span style="font-family:arial,helvetica,sans-serif">Please find attached below your recipt<br />
+        <br />
+        Username : jonathanAlderson<br />
+        Checkout&nbsp; : 15/02/19<br />
+        More detail: loren&nbsp;ipsum<br />
+        dolor eptium: pera equatus<br />
+        pectar melao: perus deulium</span>
 
-<table align="center" border="1" cellpadding="1" cellspacing="1" style="width:500px">
-	<thead>
-		<tr>
-			<th scope="col"><span style="font-family:arial,helvetica,sans-serif">Bike</span></th>
-			<th scope="col"><span style="font-family:arial,helvetica,sans-serif">Start Date</span></th>
-			<th scope="col"><span style="font-family:arial,helvetica,sans-serif">End Date</span></th>
-			<th scope="col"><span style="font-family:arial,helvetica,sans-serif">Price</span></th>
-		</tr>
-	</thead>
-	<tbody>
-		<tr>
-			<td><span style="font-family:courier new,courier,monospace">Carrera&nbsp;kraken</span></td>
-			<td><span style="font-family:courier new,courier,monospace">15/02/19</span></td>
-			<td><span style="font-family:courier new,courier,monospace">21/02/19</span></td>
-			<td><span style="font-family:courier new,courier,monospace">37.6</span></td>
-		</tr>
-		<tr>
-			<td><span style="font-family:courier new,courier,monospace">Boardman MTB 8.9</span></td>
-			<td><span style="font-family:courier new,courier,monospace">25/02/19</span></td>
-			<td><span style="font-family:courier new,courier,monospace">27/02/19</span></td>
-			<td><span style="font-family:courier new,courier,monospace">16.8</span></td>
-		</tr>
-		<tr>
-			<td>&nbsp;</td>
-			<td>&nbsp;</td>
-			<td><span style="font-family:courier new,courier,monospace">Total</span></td>
-			<td><span style="font-family:courier new,courier,monospace">54.4</span></td>
-		</tr>
-	</tbody>
-</table>
-<br />
-<br />
-<span style="font-family:arial,helvetica,sans-serif">Here is your QR code to show to a member of staff for checkout</span><br />
-<br />
-<br />
-<br />
-<span style="font-family:lucida sans unicode,lucida grande,sans-serif"><img alt="" src="https://gitlab.com/JonathanAlderson/16-cycles/wikis/uploads/7ad73b0cb1919fdf6e715ed72e66ef43/HomePage.jpg" style="float:left; height:141px; width:251px" /></span></body>
-</html>
+        <table align="center" border="1" cellpadding="1" cellspacing="1" style="width:500px">
+        	<thead>
+        		<tr>
+        			<th scope="col"><span style="font-family:arial,helvetica,sans-serif">Bike</span></th>
+        			<th scope="col"><span style="font-family:arial,helvetica,sans-serif">Start Date</span></th>
+        			<th scope="col"><span style="font-family:arial,helvetica,sans-serif">End Date</span></th>
+        			<th scope="col"><span style="font-family:arial,helvetica,sans-serif">Price</span></th>
+        		</tr>
+        	</thead>
+        	<tbody>
+        		<tr>
+        			<td><span style="font-family:courier new,courier,monospace">Carrera&nbsp;kraken</span></td>
+        			<td><span style="font-family:courier new,courier,monospace">15/02/19</span></td>
+        			<td><span style="font-family:courier new,courier,monospace">21/02/19</span></td>
+        			<td><span style="font-family:courier new,courier,monospace">37.6</span></td>
+        		</tr>
+        		<tr>
+        			<td><span style="font-family:courier new,courier,monospace">Boardman MTB 8.9</span></td>
+        			<td><span style="font-family:courier new,courier,monospace">25/02/19</span></td>
+        			<td><span style="font-family:courier new,courier,monospace">27/02/19</span></td>
+        			<td><span style="font-family:courier new,courier,monospace">16.8</span></td>
+        		</tr>
+        		<tr>
+        			<td>&nbsp;</td>
+        			<td>&nbsp;</td>
+        			<td><span style="font-family:courier new,courier,monospace">Total</span></td>
+        			<td><span style="font-family:courier new,courier,monospace">54.4</span></td>
+        		</tr>
+        	</tbody>
+        </table>
+        <br />
+        <br />
+        <span style="font-family:arial,helvetica,sans-serif">Here is your QR code to show to a member of staff for checkout</span><br />
+        <br />
+        <br />
+        <br />
+        <img src="cid:image1" alt="Your QR Code">
+        </body>
+        </html>
     """
     # Add the HTML
     emailBody = MIMEText(html, 'html')
 
+
+    # opening the image image
+    fp = open('app/qrCode.png','rb')
+    msgImage = MIMEImage(fp.read())
+    fp.close()
+
+    # defining image
+    msgImage.add_header('Content-ID','<image1>')
+    msg.attach(msgImage)
 
     msg.attach(emailBody)
 
@@ -134,4 +140,5 @@ pectar melao: perus deulium</span>
     server.sendmail(sendingAddress,receivingAddress , msg.as_string())
     server.quit()
 
+    print("Sent email")
     return render_template("qr.html")
