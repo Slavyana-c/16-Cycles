ae158509d6d34e37ac93c6eb83201fbd3ce6f654
Jonathan Alderson <sc17j3a@leeeds.ac.uk>
2019-02-27 14:38:51 +0000

    Improved Email

--- app/views.py
+++ app/views.py
@@ -43,7 +43,7 @@ def login():
 	return render_template("login.html", form=form)
 
 
-def makeHTMLTable(databaseOutput):
+def makeBikeRentalsTable(databaseOutput):
     output = ""
     for bike in databaseOutput:
         output +=  """   <tr>
@@ -56,6 +56,17 @@ def makeHTMLTable(databaseOutput):
 """
     return output
 
+def makeCheckoutTable(databaseOutput):
+    output = ""
+    titles = ["UserName","Date","Time","User ID","Total Price"]
+    for i in range(len(titles)):
+        output += """  <tr>
+    <th>""" + titles[i] + """</th>
+    <td>""" + databaseOutput[i] + """</td>
+  </tr> """
+
+    return output
+
 
 
 @app.route('/qr', methods=['GET', 'POST'])
@@ -63,15 +74,16 @@ def qr():
 
     # generating the QR code
     url = pyqrcode.create('https://ksassets.timeincuk.net/wp/uploads/sites/55/2016/07/2015_PeepShow_Mark2_Press_111115-920x610.jpg')
-    url.png('app/qrCode.png', scale=6) # nice and big
+    url.png('app/qrCode.png', scale=2) # nice and big
 
 
     # we take this dummy database output for use in the emails
-    dummyDatabaseOutput = ["Carrera Kraken","4372812","28/02/19","02/03/19","35.6",
-                          "Boardman MTB 8.8","2841849","05/04/19","08/04/19","108.2",
-                          "Apollo Storm","7394836","08/04/19","15/04/19","57.8",
-                          "Apollo Storm","7394836","08/04/19","15/04/19","57.8",]
+    dummyDatabaseOutput = [["Carrera Kraken","4372812","28/02/19","02/03/19","35.6"],
+                          ["Boardman MTB 8.8","2841849","05/04/19","08/04/19","108.2"],
+                          ["Apollo Storm","7394836","08/04/19","15/04/19","57.8"],
+                          ["Apollo Storm","7394836","08/04/19","15/04/19","57.8"]]
 
+    dummyRecieptOutput = ["Jonathan Alderson", "27/02/19","15:36:23","5437289","76.8"]
 
 
     # setting up email things
@@ -89,45 +101,21 @@ def qr():
 
     # actual body of the email
     html = """\
-
         <html>
         <head>
           <title>16 Cycles</title>
         </head>
-        <body aria-readonly="false" style="cursor: auto; font-family: arial,helvetica,sans-serif; background-image:url(cid:image1); background-repeat: no-repeat">
+        <body aria-readonly="false" style="cursor: auto; font-family: arial,helvetica,sans-serif;  background-image:url(cid:image1); background-repeat: no-repeat">
         <body>
         <br/><br/><br/>
         <h1>Thank you for renting with us today</h1>
         Please find attached below your recipt
         <br/><br/><br/><br/><br/>
         <table  align="left" border="0" cellpadding="5" cellspacing="1" style="width:800px, text-align:center, cellpadding:100px" >
-          <tr>
-            <td>UserName</td>
-            <td>jonathanAlderson</td>
-          </tr>
-          <tr>
-            <td>Date</td>
-            <td>15/02/19</td>
-          </tr>
-          <tr>
-            <td>Time</td>
-            <td>22:24.47</td>
-          </tr>
-          <tr>
-            <td>ID</td>
-            <td>5743894532</td>
-          </tr>
-          <tr>
-            <td>Price</td>
-            <td>54.4</td>
-          </tr>
+        """ + makeCheckoutTable(dummyRecieptOutput) + """
         </table>
-
-        <br/><br/><br/><br/><br/><br/>
-        <br/><br/><br/><br/><br/><br/>
-        <br/><br/><br/><br/><br/><br/>
-        <img src="cid:image2 alt="Your QR Code">
-        <img src="cid:image2 alt="Your QR Code">
+        <br/><br/><br/><br/><br/><br/><br/><br/><br/><br/>
+        <br/><br/><br/><br/><br/><br/><br/><br/><br/><br/>
         <table align="left" border="0" cellpadding="10" cellspacing="1" style="width:800px, text-align:center, cellpadding:100px" >
           <thead>
             <tr>
@@ -138,14 +126,17 @@ def qr():
               <th scope="col">Price</span></th>
             </tr>
           </thead>
-          <tbody> """ + makeHTMLTable(dummyDatabaseOutput) +
+          <tbody> """ + makeBikeRentalsTable(dummyDatabaseOutput) + """
 
-          """</tbody>
+          </tbody>
         </table>
         <br/><br/><br/><br/><br/><br/><br/><br/><br/><br/>
+        <br/><br/><br/><br/><br/><br/><br/><br/><br/><br/>
+        <br/><br/><br/><br/><br/><br/><br/><br/><br/><br/>
         Here is your QR code to show to a member of staff for checkout<br/>
-        <br/><br/><br/>
         <img src="cid:image2 alt="Your QR Code">
+        <br/><br/><br/>
+
         </body>
         </html>
     """
@@ -154,20 +145,20 @@ def qr():
     emailBody = MIMEText(html, 'html')
 
 
-#     first image
-    fp = open('app/emailBackground.png','rb')
-    msgImage = MIMEImage(fp.read())
-    fp.close()
-    msgImage.add_header('Content-ID','<image1>')
-    msg.attach(msgImage)
 
-    # second image
+    # adds the qr image
     fp = open('app/qrCode.png','rb')
     msgImage = MIMEImage(fp.read())
     fp.close()
     msgImage.add_header('Content-ID','<image2>')
     msg.attach(msgImage)
 
+    # adds the background image
+    fp = open('app/emailBackground.png','rb')
+    msgImage = MIMEImage(fp.read())
+    fp.close()
+    msgImage.add_header('Content-ID','<image1>')
+    msg.attach(msgImage)
 
 
 
