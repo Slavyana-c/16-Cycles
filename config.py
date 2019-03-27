723b64537341d7cc4fa00cc11a3e8c4365877d99
Andrew <ll16ap@leeds.ac.uk>
2019-03-27 15:36:22 +0000

    finally fixing branch

--- config.py
+++ config.py
@@ -5,6 +5,13 @@ SECRET_KEY = '39994353502789d8701e9ff62e8f86d1'
 import os
 
 
+MAIL_SERVER = 'smtp.gmail.com'
+MAIL_PORT = 587
+MAIL_USE_TLS = True
+MAIL_USERNAME = '16cyclesreset@gmail.com'
+MAIL_PASSWORD = '16cyclespword'
+
+
 basedir = os.path.abspath(os.path.dirname(__file__))
 SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
 SQLALCHEMY_TRACK_MODIFICATIONS = True
